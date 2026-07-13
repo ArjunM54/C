8a40c39f8d277672a3734921bee8c5898ffbe8d6
Arjun M <arjunabishek85@gmail.com>
2026-07-13 16:13:39 +0530

    Add files via upload

---
+++ 09.07.2026/exe10.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    printf("\Arjun\n");
+    printf("\'Arjun\'\n");
+    printf("\\Arjun\\\n");
+    printf("Arjun\rusha\n");
+    printf("Arjun\a\n");
+    printf("Arjun\bi\n");
+    printf("Arjun\tjava\n");
+    printf("Arjun\vjava");
+}
